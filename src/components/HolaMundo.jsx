470b2bd42f9455db7c0f71a4a2106a51f651a435
Jesus Zuñiga <jesuszt@hotmail.com>
2023-05-24 16:51:51 -0600

    Manejo de variables y propiedades en componentes

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -11,11 +11,30 @@ const generaTexto = () => {
     return texto;
 }
 
-const HolaMundo = () => {
+const generaNombre = () => {
+    return faker.person.firstName();
+}
+
+const generaApellidos = () => {
+    return faker.person.lastName();
+}
+
+const generaEmail = (nombre, apellidos) => {
+    return faker.internet.email({firstName: nombre, lastName: apellidos});
+}
+
+const HolaMundo = (props) => {
+
+    const nombre = props.nombre? props.nombre : generaNombre();
+    const apellidos = props.apellidos? props.apellidos : generaApellidos();
+    const email = props.email? props.email : generaEmail(nombre, apellidos);
+
     return (
         <Fragment>
             <h1>{generaTitulo()}</h1>
             <div>{generaTexto()}</div>
+            <p>Cliente: {nombre} {apellidos}</p>
+            <p>Email: {email}</p>
         </Fragment>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { HolaMundo } from "./components/HolaMundo";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HolaMundo/>
+    {/* <HolaMundo nombre = {'Jesús'} apellidos = {'Zúñiga Trejo'} email = {'zutjmx@gmail.com'}/> */}
+    <HolaMundo />
   </React.StrictMode>,
   
 )
