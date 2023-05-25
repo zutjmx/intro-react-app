3a60e259486fd990634b0f10f1d00e889d693f38
Jesus Zuñiga <jesuszt@hotmail.com>
2023-05-25 11:11:14 -0600

    Modificaciones 25/05/2023 11:10

--- src/components/HolaMundo.jsx
+++ src/components/HolaMundo.jsx
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker/locale/es_MX';
 import { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 const generaTitulo = () => {
     const titulo = faker.commerce.productName();
@@ -23,20 +24,35 @@ const generaEmail = (nombre, apellidos) => {
     return faker.internet.email({firstName: nombre, lastName: apellidos});
 }
 
-const HolaMundo = (props) => {
+const HolaMundo = ({nombre,apellidos,email,titulo,sueldo,producto}) => {
 
-    const nombre = props.nombre? props.nombre : generaNombre();
-    const apellidos = props.apellidos? props.apellidos : generaApellidos();
-    const email = props.email? props.email : generaEmail(nombre, apellidos);
+    console.log(producto);
+
+    const tituloPrincipal = titulo? titulo : generaTitulo();
+    const primerNombre = nombre? nombre : generaNombre();
+    const apPaternoMaterno = apellidos? apellidos : generaApellidos();
+    const correElectronico = email? email : generaEmail(primerNombre, apPaternoMaterno);
 
     return (
         <Fragment>
-            <h1>{generaTitulo()}</h1>
-            <div>{generaTexto()}</div>
-            <p>Cliente: {nombre} {apellidos}</p>
-            <p>Email: {email}</p>
+            <h1>{tituloPrincipal}</h1>
+            <h3>{generaTexto()}</h3>
+            <p>Cliente: {primerNombre} {apPaternoMaterno}</p>
+            <p>Email: {correElectronico}</p>
+            <h4>Sueldo bruto: {sueldo}</h4>
+            <h4>Sueldo neto: {sueldo - 5000}</h4>
+            <div>Producto: {JSON.stringify(producto)}</div>
         </Fragment>
     );
+}
+
+HolaMundo.propTypes = {
+    nombre: PropTypes.string.isRequired,
+    apellidos: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    titulo: PropTypes.string.isRequired,
+    sueldo: PropTypes.number.isRequired,
+    producto: PropTypes.object,
 }
 
 export {
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HolaMundo } from "./components/HolaMundo";
+import { faker } from '@faker-js/faker/locale/es_MX';
+
+const generaProducto = () => {
+  const descripcion = faker.commerce.product();
+  const precio = faker.number.int({min:300,max:5000});
+  const fabricante = faker.company.name();
+  const producto = {
+    descripcion,
+    precio,
+    fabricante
+  }
+  return producto;
+}
+
+const miProducto = generaProducto();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <HolaMundo nombre = {'Jesús'} apellidos = {'Zúñiga Trejo'} email = {'zutjmx@gmail.com'}/> */}
-    <HolaMundo />
+    <HolaMundo nombre = {'Jesús'} 
+               apellidos = {'Zúñiga Trejo'} 
+               email = {'zutjmx@gmail.com'}
+               sueldo = {30000}
+               titulo = {'Actuario'}
+               producto = {miProducto}
+    />
+    {/* <HolaMundo /> */}
   </React.StrictMode>,
   
 )
